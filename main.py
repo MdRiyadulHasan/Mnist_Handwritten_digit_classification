ecad25d8113df986a8b65783d69004da95761fe1
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-07-30 13:19:31 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import numpy as np
 import pandas as pd
-print('Hello world')
+import tensorflow
+import create_dataset
+from tensorflow import keras
+if __name__ == "__main__" :
+    x_train, y_train, x_test, y_test =create_dataset.load_dataset() 
